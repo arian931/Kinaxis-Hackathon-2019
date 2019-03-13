f6df45807518e3b7141430b3a30fa0cdddc3fb02
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2019-03-12 21:33:48 -0400

    bug fix push

---
+++ 2dCanvas.js
@@ -0,0 +1,42 @@
+cosnt canvas = document.getElementById('backgroundCanvas');
+const ctx = canvas.getContext('2d');
+
+let blocker = document.getElementById('he');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+ctx.fillStyle = "red";
+// ctx.fillRect(0, 0, 100, 100);
+
+let number = 0;
+let moveRate = 5;
+let rightBar = canvas.width;
+let leftBar = -canvas.width / 2;
+var image = new Image();
+image.id = "pic"
+
+setInterval(gameLoop, 10);
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (leftBar <= canvas.width / 2) {
+        ctx.clearRect(0, 0, 10000, 10000);
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "black";
+        rightBar -= moveRate;
+        leftBar += moveRate;
+        ctx.fillRect(rightBar, 0, canvas.width / 2, canvas.height / 4);
+        ctx.fillRect(leftBar, canvas.height / 4, canvas.width / 2, canvas.height / 4);
+        ctx.fillRect(rightBar, canvas.height / 2, canvas.width / 2, canvas.height / 4);
+        ctx.fillRect(leftBar, canvas.height / 4 * 3, canvas.width / 2, canvas.height / 4);
+
+        image.src = canvas.toDataURL();
+        document.getElementById('he').appendChild(image);
+    } else {
+        console.log("hello");
+        blocker.style.display = 'none';
+    }
+}
