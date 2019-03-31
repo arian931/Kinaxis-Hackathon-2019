c5feba4275d82ca9d5dc883e1ac0432bba4d5186
Master4020 <47930042+Master4020@users.noreply.github.com>
2019-03-31 19:24:44 -0400

    added powerup

---
+++ 2D Maze/Good Version/mazeEditor.js
@@ -0,0 +1,20 @@
+/*class mazeEditor{
+  constructor(x,y,width,height){
+    this.x = this.x;
+    this.y = this.y;
+    this.width = this.width;
+    this.height = this.height;
+    this.update = update
+  }
+  draw() {
+    for (let i = 0; i < 3; i++) {
+      const canvas = document.getElementById("")
+      const ctx = canvas.getContext("2d")
+      ctx.draw(random(10, 129), random(10, 129), 5, 5)
+    }
+  }
+  update(){
+    if(constructor)
+  }
+};
+*/
