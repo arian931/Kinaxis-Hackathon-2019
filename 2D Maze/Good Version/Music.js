058b25197061ce95d83c32917d234058d4b2a1ae
Master4020 <47930042+Master4020@users.noreply.github.com>
2019-03-30 21:41:16 -0400

    testing music

--- 2D Maze/Good Version/Music.js
+++ 2D Maze/Good Version/Music.js
@@ -1,9 +1,9 @@
-let i = 0
 
-if (i < 1) {
-  const music = document.getElementById("https://www.youtube.com/watch?v=CldejUAUyIs");
 
-  function playAudio() {
-    music.play();
-  }
-}
+const audio;
+function initAudioPlayer() {
+  audio = new Audio();
+  audio.src = "https://youtu.be/CldejUAUyIs";
+  audio.loop = true;
+  audio.play();
+};
