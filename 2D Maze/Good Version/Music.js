2ff37b4813706c9ebabaddfd274a64bad06757a3
Master4020 <47930042+Master4020@users.noreply.github.com>
2019-03-31 14:11:26 -0400

    :0

--- 2D Maze/Good Version/Music.js
+++ 2D Maze/Good Version/Music.js
@@ -1,5 +1,5 @@
 let i = 0
-let audio = 'C:\Users\walke\OneDrive\Documents\Ivarelli - Fast and Sad (3).mp3';
 if (i < 1) {
+  const audio = new audio('C:\Users\walke\OneDrive\Documents\Ivarelli - Fast and Sad (3).mp3');
   audio.play();
 } 
