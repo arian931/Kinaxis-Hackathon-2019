2f054ae0bad8b567e598b9836bd625a10214406b
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2019-03-31 19:58:33 -0400

    made wait time less

--- src/Hindex.js
+++ src/Hindex.js
@@ -63,7 +63,7 @@ module.exports = class miniGame {
     this.endBlurbPos = document.getElementById('TwoDRunnerPositive');
     this.endBlurbNeg = document.getElementById('TwoDRunnerNegative');
 
-    this.endBlurbDelay = 500;
+    this.endBlurbDelay = 250;
     this.endBlurbDelayCounter = 0;
 
     this.hitBlock = false;
