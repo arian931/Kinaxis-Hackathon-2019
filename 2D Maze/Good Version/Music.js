e2e034cd51ab5132a0ee7b0bc25bed393cc9316e
Master4020 <47930042+Master4020@users.noreply.github.com>
2019-03-31 17:08:09 -0400

    still

--- 2D Maze/Good Version/Music.js
+++ 2D Maze/Good Version/Music.js
@@ -1,6 +1,6 @@
 let i = 0
 if (i < 1) {
-  var audio = new audio('C:\Users\walke\OneDrive\Documents\Ivarelli - Fast and Sad (3).mp3');
+  var audio = new audio('https://youtu.be/CldejUAUyIs?list=RDCldejUAUyIs.mp3');
   audio.play();
   // var audio = new audio('Ivarelli - Fast and Sad (3).mp3');
   //audio.play();
