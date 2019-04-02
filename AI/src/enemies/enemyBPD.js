35d5c9f2fa7526762191426ad4ebaebd8355ed0c
Ali Abdoli <xXAligatorXx@outlook.com>
2019-04-02 11:06:22 -0400

    fixed AI

--- AI/src/enemies/enemyBPD.js
+++ AI/src/enemies/enemyBPD.js
@@ -3,7 +3,7 @@ const Enemy = require('./enemy');
 module.exports = class EnemyBPD extends Enemy {
   constructor(x, y, dir, mapArray) {
     super(x, y, dir, mapArray);
-    this.speed = 2.4;
+    this.speed = 10;
     this.animationSpeed = 0.1;
     this.sprite.src = '../../Art/2D/enemy_borderline_personality_disorder_spritesheet.png';
     this.animationSize = 4;
